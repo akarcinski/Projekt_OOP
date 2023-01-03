f0c824bbfb1449d6f54a50748a6f4d3831ea4719
jkr02 <jakubkroczek02@gmail.com>
2023-01-03 16:35:34 +0100

    jeszcze_tylko_checkbox_do_save

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,11 +1,16 @@
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Engine implements Runnable{
     private WorldMap mapa;
     private GameView gameView;
     private long speed;
+    private boolean saveFile=true;
     public volatile boolean flag;
     public volatile boolean terminate;
     public Engine(WorldMap mapa, GameView gameView, long speed){
@@ -21,20 +26,31 @@ public class Engine implements Runnable{
     public void run() {
         //Platform.runLater(); TUTAJ RYSOWANIE MAPY
         flag = false;
-        //System.out.println("dziala");
+        System.out.println("dziala");
         try{
+            File file = new File("src/main/resources/agh/statistics.csv");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            PrintWriter pw = new PrintWriter(file);
             while (mapa.getAnimalList().size() > 0) {
                 while (flag) {
-                    if(terminate)
+                    if(terminate){
+                        pw.close();
                         break;
-
+                    }
                     Thread.sleep(100);
                 }
 
-                if(terminate)
+                if(terminate){
+                    pw.close();
                     break;
-                //System.out.println("dziala");
-
+                }
+                System.out.println("dziala");
+                if (saveFile){
+                    pw.append(mapa.getLiveAnimalNum() + ", " + mapa.getGrassNum() + ", " + mapa.freeFields() + ", " + Arrays.toString(mapa.mostPopularGenes()) + ", " + mapa.avgEnergy() + ", " + mapa.avgLive());
+                    pw.flush();
+                }
                 mapa.cleanMap();
                 mapa.moveAnimals();
                 mapa.consumption();
@@ -42,7 +58,7 @@ public class Engine implements Runnable{
                 mapa.placeGrass();
                 mapa.changeGenesAnimals();
                 gameView.updateGrid();
-                //System.out.println("liczba fields "+mapa.freeFields());
+//                System.out.println("liczba fields "+mapa.freeFields());
                 Thread.sleep(speed);
 
             }
@@ -51,6 +67,8 @@ public class Engine implements Runnable{
             //Platform.runLater(); TUTAJ RÓWNIEŻ RYSOWANIE MAPY
         }catch(InterruptedException | FileNotFoundException interruptedException) {
             interruptedException.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
